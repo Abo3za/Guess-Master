eccf927ced567d5f08b71e0c9a263f9fa51bbeee
AbdulazizAlghamdi <ginnow24@gmail.com>
2025-03-24 21:30:42 +0300

    صورة المصارعة

--- src/components/CategorySelection.tsx
+++ src/components/CategorySelection.tsx
@@ -47,7 +47,7 @@ import React, { useState } from 'react';
       value: 'wwe',
       label: 'المصارعة',
       icon: <Sword className="w-6 h-6" />,
-      bgImage: '../public/Images/WWE.jpg'
+      bgImage: 'https://images.unsplash.com/photo-1488656711237-487ce1cc53b7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     }
   ];
   const getDifficultyDescription = (difficulty: Difficulty, category: Category | null) => {
